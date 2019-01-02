7ccf3c115bceee090a87107e3da0e9fb2e05ecb4
csPython <cspython@AD25TF04NGF1J-64.local>
2019-01-02 12:58:35 -0500

    Second Version: Create Question and Answers

--- questionSG.py
+++ questionSG.py
@@ -1,7 +1,14 @@
-#Create 1 Question 4 Option Code
+#Create 1 Question 4 Option Code5
 
 #Identify Variables
 question = int()
 choice = False
 correct = 0
 
+#Question
+question = int(input("""Which one of these colours are the primary colours?
+1. Yellow
+2. Green
+3. Orange
+4. Purple"""))
+print()
