930ec062a8d0175d05e32184a0ddd3988e3004d6
csPython <cspython@AD25TF04NGF1J-64.local>
2019-01-07 10:56:24 -0500

    Third Version: Adds Loop Into Question

--- questionSG.py
+++ questionSG.py
@@ -1,14 +1,30 @@
-#Create 1 Question 4 Option Code5
+#Create While Loop
 
 #Identify Variables
-question = int()
+q1 = int()
+q1t = """Which one of these colours are the primary colours?
+1.Yellow
+2.Green
+3.Orange
+4.Purple
+
+Answer:  """
 choice = False
 correct = 0
 
-#Question
-question = int(input("""Which one of these colours are the primary colours?
-1. Yellow
-2. Green
-3. Orange
-4. Purple"""))
-print()
+#While Loop
+while choice == False:
+    try:
+        q1 = int(input(q1t))
+        if q1 == 1:
+            correct += 1
+            choice == True
+
+        elif 0 < q1 < 5:
+            choice == True
+
+        else:
+            print("Please type a positive number between 1 and 4. ")
+
+    except ValueError:
+        print("Seriously? Please put a WHOLE POSITIVE INTEGER between 1 and 4. ")
